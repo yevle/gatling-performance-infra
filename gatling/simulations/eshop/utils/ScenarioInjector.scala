93acfef436149ebaf6c06781ab3bd1f7d64d5355
SviatoslavPaseka <paseka.syat@gmail.com>
2023-11-21 17:11:39 +0200

    refactoring1

--- gatling/simulations/eshop/utils/ScenarioInjector.scala
+++ gatling/simulations/eshop/utils/ScenarioInjector.scala
@@ -6,11 +6,18 @@ import io.gatling.core.controller.inject.closed.ClosedInjectionStep
 import io.gatling.core.controller.inject.open.OpenInjectionStep
 
 object ScenarioInjector {
-  def injectOpenModel (usersRatePerSecond: Int, rampUpDurationSeconds: FiniteDuration, steadyStateDurationSeconds: FiniteDuration)
+  def injectComplexOpenModel (usersRatePerSecond: Int, rampUpDurationSeconds: FiniteDuration, steadyStateDurationSeconds: FiniteDuration)
     : Seq[OpenInjectionStep] = {
     Seq.apply(
       rampUsersPerSec(0) to usersRatePerSecond during rampUpDurationSeconds,
       constantUsersPerSec(usersRatePerSecond) during steadyStateDurationSeconds
+    )
+  }
+
+  def injectOpenModel(userCount: Int, rampUpDuration: FiniteDuration)
+  : Seq[OpenInjectionStep] = {
+    Seq.apply(
+      rampUsers(userCount) during rampUpDuration
     )
   }
 
